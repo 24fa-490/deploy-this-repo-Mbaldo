7b1efd04f0e44e4168bb34ade719324ee1a86427
Michael Baldo <baldo_michael@yahoo.com>
2024-10-27 16:23:40 -0700

    Fix vite config for production on Heroku

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,16 @@
-import mkcert from "vite-plugin-mkcert"
+import mkcert from 'vite-plugin-mkcert';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
-
-export default defineConfig({
-	plugins: [sveltekit(),
-		mkcert()
-	],
-	server: {
-		https: true,
-		proxy: {}, // essential to avoid "can't use Symbol where you need a string" error
-	},
-})
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [
+      sveltekit(),
+      mode === 'development' && mkcert()  // Only apply mkcert in development
+    ].filter(Boolean),  // Ensure no false or undefined plugins
+    server: {
+      https: mode === 'development',  // Use HTTPS only in development
+      proxy: mode === 'development' ? {} : undefined // Only set proxy in development
+    }
+  };
+});
